d3781de1e6e07f2c8700786abda8c80e20d23c3b
Himanshu Gangwar <151658698+zenitsu0509@users.noreply.github.com>
2024-11-02 17:53:12 +0530

    Update testing_model.py

--- Face_Emotion_model/testing_model.py
+++ Face_Emotion_model/testing_model.py
@@ -103,6 +103,5 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
-# Release the webcam and close windows
 cap.release()
 cv2.destroyAllWindows()
